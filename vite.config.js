c8c4d0bfa516710c336558c8fdc74d3700920d3e
Erick Tucto <erick@ericktucto.com>
2024-09-26 00:08:41 -0500

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteSingleFile()
   ],
+  base: "https://ericktucto.com/invitacion-a-boda"
 })
